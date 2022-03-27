02ae703abc2da312a077a1567836daa5549bfa9e
Ayda Akdoğan <77860020+arayda1@users.noreply.github.com>
2022-03-27 20:47:48 +0200

    Create task1.cs

---
+++ task1.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+           int Studentno = 44802;
+            Console.WriteLine(Studentno);
+            Console.ReadLine();
+        }
+    }
+}
